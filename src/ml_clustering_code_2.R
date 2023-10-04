4a762a268733a765573f10e87166f1b82f74b790
JihoonYun <yun2000k@gmail.com>
2023-10-04 15:43:41 -0600

    Add files via upload

--- src/ml_clustering_code_2.R
+++ src/ml_clustering_code_2.R
@@ -8,7 +8,7 @@ scaled_org_data <- read.csv("used_cars_data_cleaned_final_ver_sampled.csv")
 #scaled_data <- read.csv("used_cars_data_cleaned_final_ver_sampled_preprocessed.csv")
 
 # Set seed for reproducibility
-set.seed(123)
+set.seed(2010)
 
 # Number of rows to sample 
 num_rows_to_sample <- 100
@@ -17,38 +17,36 @@ num_rows_to_sample <- 100
 sampled_org_data <- scaled_org_data[sample(nrow(scaled_org_data), num_rows_to_sample, replace = FALSE), ]
 
 # Extract the desired columns from scaled_org_data
-sampled_data <- sampled_org_data[, c('horsepower', 'engine_displacement', 'mileage', 'year')]
-
+sampled_data <- sampled_org_data[, c('horsepower', 'engine_cylinders', 'mileage', 'year', 'price')]
+sampled_data2 <- sampled_org_data
 # Convert 'has_accidents' column from strings to 1 for True and 0 for False, and then to numeric
-#sampled_data$has_accidents <- as.numeric(sampled_data$has_accidents == "True")
+# sampled_data$has_accidents <- as.numeric(sampled_data$has_accidents == "True")
 
 # Scaling Data
 sampled_data <- scale(sampled_data)
+sampled_data
 
 # Calculate average value for each cluster
 cosine_distance <- proxy::dist(as.matrix(sampled_data), method = "cosine")
 
 # Perform hierarchical clustering
-hierarchical_clustering <- hclust(cosine_distance, method = "ward.D2") 
+hierarchical_clustering <- hclust(cosine_distance, method = "ward.D") 
 
 ########
 # Create a cluster with K set to 3
-k <-4
+k <-3
 clusters <- cutree(hierarchical_clustering, k)
 
-# Label settings
-#labels <- round(sampled_org_data$price / 1000, digits = 2)#rownames(sampled_data)
-labels <- paste(sampled_org_data$model_name, '/', sampled_org_data$year)
-# unique_breaks <- unique(c(min(sampled_org_data$price), quantile(sampled_org_data$price, c(0, 0.33, 0.66, 1)), max(sampled_org_data$price)))
-# labels <- cut(
-#   sampled_org_data$price,
-#   breaks = unique_breaks,
-#   labels = c("HIGH PRICE", "MEDIUM PRICE", "LOW PRICE"),
-#   include.lowest = TRUE
-# )
+unique_breaks <- unique(c(min(sampled_data2$price), quantile(sampled_data2$price, c(0, 0.3, 0.7, 1)), max(sampled_data2$price)))
+labels <- cut(
+  sampled_data2$price,
+  breaks = unique_breaks,
+  labels = c("LOW PRICE", "MEDIUM PRICE", "HIGH PRICE"),
+  include.lowest = TRUE
+)
 
 # Draw a dendrogram (show clusters)
-plot(hierarchical_clustering, labels = labels, cex = 0.6)
+plot(hierarchical_clustering, labels = labels, cex = 0.7)
 
 # Show K clusters (colored)
 rect.hclust(hierarchical_clustering, k = k, border = 'blue')
